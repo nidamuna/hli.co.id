0018d7329c0a4eab8aa5b391fa9446cf2a43aa2a
nidamuna <67530654+nidamuna@users.noreply.github.com>
2022-09-18 20:04:41 +0700

    install tailwindcss

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from './components/Header';
+import Counter from './components/Counter'
+import Course from './components/Course'
+import Category from './components/Category'
+import Process from './components/Process';
+import Features from './components/Features';
+import Team from './components/Team';
+import Testimonial from './components/Testimonial';
+import Join from './components/Join';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Counter />
+      <Course />
+      <Category />
+      <Process />
+      <Features />
+      <Team />
+      <Testimonial />
+      <Join />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
